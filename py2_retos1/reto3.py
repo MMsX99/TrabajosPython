5ebbaae74239a3d39dc09985b68703b849e40c12
MMsX99 <>
2021-04-22 19:50:43 +0000

    reto 2 y 3 terminados, no llego notificacion del reto 2 asi que elimine y volvi a crear el webhook

---
+++ py2_retos1/reto3.py
@@ -0,0 +1,10 @@
+a=input('ingrese un primer numero: ')
+b=input('ingrese un segundo numero: ')
+c=a+b
+print('suma de los numeros: ',c)
+c=a-b
+print('resta de los numeros: ',c)
+c=a*b
+print('multiplicación de los numeros: ',c)
+c=a/b
+print('division de los numeros: ',c)
