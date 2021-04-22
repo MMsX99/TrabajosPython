f08ab35d94774d292a5905aa102255ffa9b3a06e
MMsX99 <>
2021-04-22 20:46:28 +0000

    reto 4 listo

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,8 @@
+a=int(input('ingrese un primer numero: '))
+b=int(input('ingrese un segundo numero: '))
+if a==b:
+  print('los numeros son iguales')
+if a>b:
+  print(a,' es mayor que ',b)
+if a<b:
+  print(b,' es mayor que ',a)
