078aebf55829401f0b7627a6811e32336aba3f26
MMsX99 <>
2021-04-22 21:44:06 +0000

    reto 20

--- py2_retos1/reto20.py
+++ py2_retos1/reto20.py
@@ -2,10 +2,10 @@ def bisiesto(a):
   if a%4 == 0:
     if a%100 == 0:
       if a%400 == 0:
-        mensaje='el año es bisiesto'
-      else: mensaje='el año no es bisiesto'
-    else: mensaje='el año es bisiesto'
-  else:mensaje='el año no es bisiesto'
+        mensaje='el año es bisiesto.'
+      else: mensaje='el año no es bisiesto.'
+    else: mensaje='el año es bisiesto.'
+  else:mensaje='el año no es bisiesto.'
   return mensaje
 A=int(input('ingrese un año para saber si es bisiesto: '))
 print(bisiesto(A))
