7135fe980375f0935f147222e5c009ef3294ad17
Miguel Montero <82407362+MMsX99@users.noreply.github.com>
2021-08-11 22:48:00 +0000

    creacion carpeta y archivo py5

---
+++ py5_matplotlib/ plot_list.py
@@ -0,0 +1,12 @@
+import matplotlib
+Lprueba=[]
+print("programa que calcula una lista de números y luego grafica la lista utilizando matplotlib")
+n=input("ingrese el primer numero de la lista: ")
+while(n != 1):
+  Lprueba.append(n)
+  if(n%2 == 0):
+    n=n/2
+  else:
+
+
+} 
