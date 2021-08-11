0a0aed737b4b3e845782d923935bb8f1c4f3e15e
Miguel Montero <82407362+MMsX99@users.noreply.github.com>
2021-08-11 23:27:48 +0000

    avance plot_list

--- py5_matplotlib/plot_list.py
+++ py5_matplotlib/plot_list.py
@@ -1,4 +1,4 @@
-import matplotlib
+import matplotlib.pylot as plt
 Lprueba=[]
 print("programa que calcula una lista de números y luego grafica la lista utilizando matplotlib")
 n=input("ingrese el primer numero de la lista: ")
@@ -7,6 +7,7 @@ while(n != 1):
   if(n%2 == 0):
     n=n/2
   else:
-
-
-} 
+    n=(n*3)+1
+    
+plt.plot(Lprueba)
+plt.show()
